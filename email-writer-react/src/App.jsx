2ffe31a352f6a0a9be42e027fb0db1a7e12e8aee
Blagovest Kabov <kabov.blg@gmail.com>
2025-05-01 20:59:15 +0300

    frontend inital structure

--- email-writer-react/src/App.jsx
+++ email-writer-react/src/App.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react'
 import './App.css'
+import { Container, TextField, Typography,Box } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [emailContent, setEmailContent] = useState('');
+  const [tone, setTone] = useState('');
+  const [generatedReply, setGeneratedReply] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
 
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <Container maxWidth='md' sx={{py:4}}>
+      <Typography variant='h3' component='h1' gutterButton>
+        Email Reply Generator
+      </Typography>
+
+      <Box sx={{ mx: 3}}>
+        <TextField
+          fullWidth
+          multiline
+          rows={6}
+          variant='outlined'
+          label="Initial Email Content"
+          value={emailContent || ''}
+          onChange={(e) => setEmailContent(e.target.value)}
+          sx={{ mb:2 }}/>
+      </Box>
+    </Container>
   )
 }
 
